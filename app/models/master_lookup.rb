0d2897b6cb2d7bf3d32303b78da6ed66495aa699
Muneeb <muneeb@ginzametrics.com>
2019-10-21 21:56:40 +0500

    models are addrd

---
+++ app/models/master_lookup.rb
@@ -0,0 +1,5 @@
+class MasterLookup < ApplicationRecord
+  def self.login(browser, email, password)
+
+  end
+end
---
+++ db/migrate/20191021165438_create_job_posts.rb
@@ -0,0 +1,12 @@
+class CreateJobPosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :job_posts do |t|
+      t.string :title
+      t.string :company
+      t.string :url
+      t.jsonb :settings
+
+      t.timestamps
+    end
+  end
+end
